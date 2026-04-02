16105f52a0f24714d4fbd755eb96347439c58dae
Fardan777 <muhammad_alfardan_34rpl@student.smktelkom-mlg.sch.id>
2026-04-02 13:19:27 +0700

    Add files via upload

---
+++ materi.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>tugas muhammad nasywan alfardan xrp7 absen 31</h1>
+    <p>Buat list 5 teman yang berisi data nama dan foto</p>
+
+    <p>X RPL 7</p>
+    <ol type="1">
+        <li>zaki</li>
+        <img src="zaki.jpeg" alt="ini zaki" width="200" height="200">
+        <li>vio</li>
+        <img src="vio.jpeg" alt="ini vio" width="200" height="200">
+        <li>jakcy</li>
+        <img src="jackky.jpeg" alt="ini jeki" width="200" height="200">
+        <li>yayan</li>
+        <img src="yayan.jpeg" alt="ini yayanto" width="200" height="200">
+        <li>rahman</li>
+        <img src="rahman.jpeg" alt="ini rahman" width="200" height="200">
+    </ol>
+</body>
+</html>
